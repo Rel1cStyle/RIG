32c33623da7008488b845c1d17467ee79c272984
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-10-26 04:23:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from os import getenv
-
 class App():
-    def __init__(self):
-        App.name = "Rel1cStyle Render Image Gallery"
-        App.version = "1.0"
-        with open("_commit_sha.txt", mode="r") as f:
-            App.commit_sha = f.read()
+	name = "Rel1cStyle Render Image Gallery"
+	version = "1.0"
+	try:
+		with open("_commit_sha.txt", mode="r") as f:
+			commit_sha = f.read()
+	except Exception:
+		commit_sha = "dev"
