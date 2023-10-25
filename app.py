f21656110ec361f2aadd8defec48ed2471fb7dca
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-10-26 04:43:08 +0900

    スライスの値を修正

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class App():
 
 	if path.isfile("_commit_sha.txt"):
 		with open("_commit_sha.txt", mode="r") as f:
-			commit_sha = f.read()[0:6]
+			commit_sha = f.read()[0:7]
 	else:
 		commit_sha = "0"
 
