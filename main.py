c91738670c6dd3333f01c3db0d09c9e1fe77858e
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-10-26 19:07:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -201,13 +201,7 @@ async def main(page: ft.Page):
 		actions=[
 			# バージョン表記テキスト
 			ft.Container(
-				ft.Text(f"Branch: {App.branch}", size=12, text_align=ft.TextAlign.RIGHT),
-				padding=ft.padding.only(0, 18, 20, 0),
-				alignment=ft.alignment.center_right,
-				expand=False
-			),
-			ft.Container(
-				ft.Text(f"Commit: {App.commit_sha}", size=12, text_align=ft.TextAlign.RIGHT),
+				ft.Text(f"Branch: {App.branch} | Commit: {App.commit_sha}", size=12, text_align=ft.TextAlign.RIGHT),
 				padding=ft.padding.only(0, 0, 20, 0),
 				alignment=ft.alignment.center_right,
 				expand=False
