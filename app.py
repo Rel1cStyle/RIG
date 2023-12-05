6821ed55a3e84fdd2a6f3d991dc58b4f7a975690
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-12-05 17:04:08 +0900

    コミットのハッシュとブランチから改行を削除するように改良

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ class App():
 	if path.isfile("_commit_sha.txt"):
 		with open("_commit_sha.txt", mode="r") as f:
 			commit_sha = f.read()[0:7]
+			commit_sha = commit_sha.replace("\n", "")
 	else:
 		commit_sha = "0"
 
 	if path.isfile("_branch_name.txt"):
 		with open("_branch_name.txt", mode="r") as f:
 			branch = f.read()
+			branch = branch.replace("\n", "")
 	else:
 		branch = "dev"
