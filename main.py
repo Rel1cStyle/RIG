fdd93ca7d953b884b38c8707b4eb9a94133bc9de
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2024-04-02 00:12:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1178,4 +1178,4 @@ async def main(page: ft.Page):
 	await page.update_async()
 
 
-ft.app(target=main, assets_dir="assets", port=8000, view=ft.AppView.FLET_APP, web_renderer=ft.WebRenderer.HTML)
+ft.app(target=main, assets_dir="assets", port=8000)
