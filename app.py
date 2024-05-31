3d3116aee4a84640d6b7b5df0398c26770f01ea1
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2024-05-31 17:04:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,12 @@
-from os import environ, path
+from os import getenv
 
 class App():
 	NAME = "Rel1cStyle RIG"
 
 	VERSION = "0.2.1"
 
-	#API_URL = "https://api.rig.rel1c.work"
 	API_URL = "https://rig-api-thunder.rel1c.work"
-	#preview_image_url = "https://rig-r2-images.huerisalter.com"
 
-	COMMIT_SHA = ""
-	BRANCH = ""
-	ENV = ""
-
-	if path.isfile("_commit_sha.txt"):
-		with open("_commit_sha.txt", mode="r") as f:
-			COMMIT_SHA = f.read()[0:7].replace("\n", "")
-	else:
-		COMMIT_SHA = "0"
-
-	if path.isfile("_branch_name.txt"):
-		with open("_branch_name.txt", mode="r") as f:
-			BRANCH = f.read().replace("\n", "")
-	else:
-		BRANCH = "dev"
-
-	if path.isfile("_env_name.txt"):
-		with open("_env_name.txt", mode="r") as f:
-			ENV = f.read().replace("\n", "")
-	else:
-		ENV = "UNKNOWN"
+	COMMIT_SHA = os.getenv("SOURCE_COMMIT")
+	BRANCH = os.getenv("COOLIFY_BRANCH")
+	ENV = os.getenv("DYNA")
